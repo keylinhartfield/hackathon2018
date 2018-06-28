16013f941914ff1575943caf3f579bc38f9beff8
Alex Akunevich <alexander.okunevich@gmail.com>
2018-06-28 12:16:39 -0500

    commit

--- RPi-2-leds/src/main/java/hackathon/rpi2/LedControl.java
+++ RPi-2-leds/src/main/java/hackathon/rpi2/LedControl.java
@@ -47,27 +47,7 @@ public class LedControl {
         pin.low();
         System.out.println("--> GPIO state should be: OFF");
 
-        Thread.sleep(5000);
-
-        // toggle the current state of gpio pin #01 (should turn on)
-        pin.toggle();
-        System.out.println("--> GPIO state should be: ON");
-
-        Thread.sleep(5000);
-
-        // toggle the current state of gpio pin #01  (should turn off)
-        pin.toggle();
-        System.out.println("--> GPIO state should be: OFF");
-
-        Thread.sleep(5000);
-
-        // turn on gpio pin #01 for 1 second and then off
-        System.out.println("--> GPIO state should be: ON for only 1 second");
-        pin.pulse(1000, true); // set second argument to 'true' use a blocking call
-
-        // stop all GPIO activity/threads by shutting down the GPIO controller
-        // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
-        gpio.shutdown();
+         gpio.shutdown();
 
         System.out.println("Exiting ControlGpioExample");
 
@@ -93,26 +73,6 @@ public class LedControl {
         pin.low();
         System.out.println("--> GPIO state should be: OFF");
 
-        Thread.sleep(5000);
-
-        // toggle the current state of gpio pin #01 (should turn on)
-        pin.toggle();
-        System.out.println("--> GPIO state should be: ON");
-
-        Thread.sleep(5000);
-
-        // toggle the current state of gpio pin #01  (should turn off)
-        pin.toggle();
-        System.out.println("--> GPIO state should be: OFF");
-
-        Thread.sleep(5000);
-
-        // turn on gpio pin #01 for 1 second and then off
-        System.out.println("--> GPIO state should be: ON for only 1 second");
-        pin.pulse(1000, true); // set second argument to 'true' use a blocking call
-
-        // stop all GPIO activity/threads by shutting down the GPIO controller
-        // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
         gpio.shutdown();
 
         System.out.println("Exiting ControlGpioExample");
