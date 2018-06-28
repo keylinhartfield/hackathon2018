52782daac0465fc1ca89ce7e286c4e9a4769fc22
Alex Akunevich <alexander.okunevich@gmail.com>
2018-06-28 13:39:46 -0500

    commit

--- RPi-2-leds/src/main/java/hackathon/rpi2/Runner.java
+++ RPi-2-leds/src/main/java/hackathon/rpi2/Runner.java
@@ -2,9 +2,12 @@ package hackathon.rpi2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        GsonAutoConfiguration.class
+})
 @EnableScheduling
 public class Runner {
 
