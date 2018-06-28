f2dedcd9dd57a82302e4477ffbf769a204f93abe
Alex Akunevich <alexander.okunevich@gmail.com>
2018-06-28 15:24:16 -0500

    commit

--- RPi-1/src/main/java/jpmc/hackathon/rpi1/Runner.java
+++ RPi-1/src/main/java/jpmc/hackathon/rpi1/Runner.java
@@ -2,8 +2,10 @@ package jpmc.hackathon.rpi1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Runner {
 
     public static void main(String[] args) {
